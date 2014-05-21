c8965ed5785499fdc06eb78c338a3adda212503e
David Crees <davidc@Minke.local>
2014-05-22 00:46:33 +0100

    Only create redirect record if permalink field has changed

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,8 +3,11 @@ module Spree
     after_save :save_permalink_to_history
     
     def save_permalink_to_history
-      puts "Saving old value #{permalink_was}"
-      ProductRedirect.create(:permalink => permalink_was, :product => self)
+      if permalink_changed?
+        #puts "Saving redirect for old permalink value #{permalink_was}"
+        ProductRedirect.create(:permalink => permalink_was, :product => self)
+      end
     end
-  end    
+  end
 end
+
